587ab709593da45b64d35ea1eaebd3c9e119f6c6
Mario Krizmanic <mario.krizmanic@gmail.com>
2013-10-22 21:10:45 +0200

    Added initial panifex-platform-api bundle

---
+++ panifex-parent/panifex-platform-api/src/main/java/org/panifex/platform/api/ExampleService.java
@@ -0,0 +1,12 @@
+package org.panifex.platform.api;
+
+/**
+ * Public API representing an example OSGi service
+ */
+public interface ExampleService
+{
+    // public methods go here...
+
+    String scramble( String text );
+}
+
