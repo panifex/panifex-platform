a71521e235e462268cb1edc96dd222508d37b2dd
Mario Krizmanic <mario.krizmanic@gmail.com>
2013-10-26 09:22:00 +0200

    Created initial panifex-platform-web-impl bundle

---
+++ panifex-parent/panifex-platform-web-impl/src/main/java/org/panifex/platform/web/impl/ExampleService.java
@@ -0,0 +1,12 @@
+package org.panifex.platform.web.impl;
+
+/**
+ * Public API representing an example OSGi service
+ */
+public interface ExampleService
+{
+    // public methods go here...
+
+    String scramble( String text );
+}
+
