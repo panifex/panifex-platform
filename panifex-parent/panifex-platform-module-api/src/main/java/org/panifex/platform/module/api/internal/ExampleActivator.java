af8c4fcac996ca1649d62e8126257b511e38b522
Mario Krizmanic <mario.krizmanic@gmail.com>
2013-10-27 10:23:48 +0100

    Added initial Panifex Platform Module Api bundle

---
+++ panifex-parent/panifex-platform-module-api/src/main/java/org/panifex/platform/module/api/internal/ExampleActivator.java
@@ -0,0 +1,35 @@
+package org.panifex.platform.module.api.internal;
+
+import java.util.Dictionary;
+import java.util.Properties;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * Extension of the default OSGi bundle activator
+ */
+public final class ExampleActivator
+    implements BundleActivator
+{
+    /**
+     * Called whenever the OSGi framework starts our bundle
+     */
+    public void start( BundleContext bc )
+        throws Exception
+    {
+        System.out.println( "STARTING org.panifex.platform.module.api" );
+
+    }
+
+    /**
+     * Called whenever the OSGi framework stops our bundle
+     */
+    public void stop( BundleContext bc )
+        throws Exception
+    {
+        System.out.println( "STOPPING org.panifex.platform.module.api" );
+
+    }
+}
+
