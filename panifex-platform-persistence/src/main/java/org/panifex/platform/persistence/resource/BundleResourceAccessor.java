7650aec421e813183953da906cf27ddc8f79c2a7
Mario Krizmanic <mario.krizmanic@gmail.com>
2013-11-05 19:36:44 +0100

    Added core liquibase support

---
+++ panifex-platform-persistence/src/main/java/org/panifex/platform/persistence/resource/BundleResourceAccessor.java
@@ -0,0 +1,36 @@
+package org.panifex.platform.persistence.resource;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Enumeration;
+
+import org.osgi.framework.BundleContext;
+
+import liquibase.resource.ResourceAccessor;
+
+public class BundleResourceAccessor implements ResourceAccessor {
+
+	private BundleContext context;
+	
+	public BundleResourceAccessor(BundleContext context) {
+		this.context = context;
+	}
+	
+	@Override
+	public InputStream getResourceAsStream(String file) throws IOException {
+		URL resource = context.getBundle().getResource(file);
+		return resource.openStream();
+	}
+
+	@Override
+	public Enumeration<URL> getResources(String packageName) throws IOException {
+		return context.getBundle().getResources(packageName);
+	}
+
+	@Override
+	public ClassLoader toClassLoader() {
+		return null;
+	}
+
+}
