4a920fbcf9df203b7b80c537780c7eac955e82f7
Mario Krizmanic <mario.krizmanic@gmail.com>
2014-01-01 15:50:26 +0100

    Adds SecurityServiceManager which listens to SecurityService implementations.

---
+++ panifex-web-impl/src/main/java/org/panifex/web/impl/security/SecurityServiceManager.java
@@ -0,0 +1,118 @@
+/*******************************************************************************
+ * Panifex platform
+ * Copyright (C) 2013  Mario Krizmanic
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ ******************************************************************************/
+package org.panifex.web.impl.security;
+
+import org.apache.aries.blueprint.annotation.Bean;
+import org.apache.aries.blueprint.annotation.Bind;
+import org.apache.aries.blueprint.annotation.Destroy;
+import org.apache.aries.blueprint.annotation.Inject;
+import org.apache.aries.blueprint.annotation.Reference;
+import org.apache.aries.blueprint.annotation.ReferenceListener;
+import org.apache.aries.blueprint.annotation.Unbind;
+import org.panifex.service.api.security.SecurityService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Bean(id = SecurityServiceManager.ID, factoryMethod = "init")
+@ReferenceListener
+public final class SecurityServiceManager {
+
+    public static final String ID = "org.panifex.web.impl.security.SecurityServiceManager";
+    
+    private static final Logger log = LoggerFactory.getLogger(SecurityServiceManager.class);
+    
+    /**
+     * The singleton instance of {@link SecurityServiceManager} class.
+     */
+    private static volatile SecurityServiceManager manager;
+
+    /**
+     * The reference of the {@link org.panifex.service.api.security.SecurityService SecurityService}.
+     * The reference is dynamically updated when the implementations of the {@link org.panifex.service.api.security.SecurityService SecurityService}
+     * appear or disappear.
+     */
+    @Inject
+    @Reference(
+        serviceInterface = SecurityService.class,
+        referenceListeners = @ReferenceListener(ref = ID))
+    private SecurityService securityService;
+    
+    /**
+     * Private construct which protects any other class from instantiating.
+     */
+    private SecurityServiceManager() {
+    }
+    
+    /**
+     * Factory method which initializes {@link SecurityServiceManager}.
+     * 
+     * @return the {@link SecurityServiceManager} singleton instance
+     */
+    public static SecurityServiceManager init() {
+        log.debug("Initialize SecurityServiceManager");
+        if (manager == null) {
+            synchronized(SecurityServiceManager.class) {
+                if (manager == null) {
+                    manager = new SecurityServiceManager();
+                }
+            }
+        }
+        return manager;
+    }
+    
+    /**
+     * Frees linked objects.
+     */
+    @Destroy
+    public void destroy() {
+        manager = null;
+    }
+    
+    /**
+     * Binds {@link org.panifex.service.api.security.SecurityService SecurityService} to the manager.
+     * 
+     * @param securityService the appeared {@link org.panifex.service.api.security.SecurityService SecurityService}
+     */
+    @Bind
+    public void bind(SecurityService securityService) {
+        log.debug("Bind SecurityService: {}", securityService);
+        this.securityService = securityService;
+    }
+    
+    /**
+     * Unbinds {@link org.panifex.service.api.security.SecurityService SecurityService} from the manager
+     * 
+     * @param securityService the disappeared {@link org.panifex.service.api.security.SecurityService SecurityService}
+     */
+    @Unbind
+    public void unbind(SecurityService securityService) {
+        log.debug("Unbind SecurityService: {}", securityService);
+        this.securityService = null;
+    }
+    
+    /**
+     * Returns the {@link org.panifex.service.api.security.SecurityService SecurityService},
+     * or null if it does not exist.
+     * 
+     * @return the {@link org.panifex.service.api.security.SecurityService SecurityService}, or null if it does not exist
+     */
+    public static SecurityService getService() {
+        return manager.securityService;
+    }
+}
