d30e2bb43ff001705a16878b51de0ca99b99ca02
Mario Krizmanic <mario.krizmanic@gmail.com>
2014-01-11 11:50:41 +0100

    Adds javadoc comments to AppMenuServiceImpl
    Signed-off-by: Mario Krizmanic <mario.krizmanic@gmail.com>

--- panifex-web-impl/src/main/java/org/panifex/web/impl/menu/AppMenuServiceImpl.java
+++ panifex-web-impl/src/main/java/org/panifex/web/impl/menu/AppMenuServiceImpl.java
@@ -32,6 +32,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.zkoss.zul.TreeNode;
 
+/**
+ * An {@link org.panifex.module.api.menu.AppMenuService} implementation
+ * for dynamic managing of the application menu.
+ * <p>
+ * Internally, it contains a {@link MenuTreeModel model} and it binds and unbinds
+ * the {@link org.panifex.module.api.menu.MenuAction MenuAction}s and the
+ * {@link org.panifex.module.api.menu.MenuNode MenuNode}s from the model.
+ *
+ */
 @Bean(id = AppMenuServiceImpl.ID)
 @Service(interfaces = AppMenuService.class, 
     registerationListeners = @RegistrationListener(ref = AppMenuManager.ID))
@@ -42,48 +51,99 @@ public final class AppMenuServiceImpl implements AppMenuService, Serializable {
      */
     private static final long serialVersionUID = 652695490192648820L;
 
+    /**
+     * The {@link org.apache.aries.blueprint.annotation.Bean Bean Bean}'s ID
+     * which represents it in a blueprint container.
+     */
     public static final String ID = "org.panifex.web.impl.menu.AppMenuServiceImpl";
     
-    private Logger log = LoggerFactory.getLogger(AppMenuServiceImpl.class);
+    /**
+     * The logger
+     */
+    private final Logger log = LoggerFactory.getLogger(AppMenuServiceImpl.class);
     
-    private MenuTreeModel model = new MenuTreeModel();
+    /**
+     * The {@link MenuTreeModel} in which the menu items will be binded to.
+     */
+    private final MenuTreeModel model = new MenuTreeModel();
     
+    /**
+     * Binds the {@link org.panifex.module.api.menu.MenuAction MenuAction} 
+     * to the {@link AppMenuServiceImpl#model model}.
+     * 
+     * @param the {@link org.panifex.module.api.menu.MenuAction MenuAction} to be binded
+     */
     @Override
     public void bindMenuAction(MenuAction menuAction) {
         log.debug("Bind MenuAction: {}", menuAction);
         bindItem(menuAction);
     }
 
+    /**
+     * Binds the {@link org.panifex.module.api.menu.MenuNode MenuNode}
+     * to the {@link AppMenuServiceImpl#model model}.
+     * 
+     * @param the {@link org.panifex.module.api.menu.MenuNode MenuNode} to be binded
+     */
     @Override
     public void bindMenuNode(MenuNode menuNode) {
         log.debug("Bind MenuNode: {}", menuNode);
         bindItem(menuNode);
     }
 
+    /**
+     * Binds the {@link org.panifex.module.api.menu.MenuItem MenuItem}
+     * to the {@link AppMenuServiceImpl#model model}.
+     * 
+     * @param item the {@link org.panifex.module.api.menu.MenuItem MenuItem} to be binded
+     */
     private void bindItem(MenuItem item) {
         synchronized (model) {
             model.addItem(item);
         }
     }
-        
+
+    /**
+     * Unbinds the {@link org.panifex.module.api.menu.MenuAction MenuAction} 
+     * from the {@link AppMenuServiceImpl#model model}.
+     * 
+     * @param the {@link org.panifex.module.api.menu.MenuAction MenuAction} to be unbinded
+     */
     @Override
     public void unbindMenuAction(MenuAction menuAction) {
         log.debug("Unbind MenuAction: {}", menuAction);
         unbindItem(menuAction);
     }
 
+    /**
+     * Unbinds the {@link org.panifex.module.api.menu.MenuNode MenuNode}
+     * from the {@link AppMenuServiceImpl#model model}.
+     * 
+     * @param the {@link org.panifex.module.api.menu.MenuNode MenuNode} to be unbinded
+     */
     @Override
     public void unbindMenuNode(MenuNode menuNode) {
         log.debug("Unbind MenuNode: {}", menuNode);
         unbindItem(menuNode);
     }
 
+    /**
+     * Unbinds the {@link org.panifex.module.api.menu.MenuItem MenuItem}
+     * from the {@link AppMenuServiceImpl#model model}.
+     * 
+     * @param item the {@link org.panifex.module.api.menu.MenuItem MenuItem} to be unbinded
+     */
     private void unbindItem(MenuItem item) {
         synchronized (model) {
             model.removeItem(item);
         }
     }
 
+    /**
+     * Return the collection of {@link org.panifex.module.api.menu.MenuItem MenuItem}.
+     * 
+     * @return the collection of {@link org.panifex.module.api.menu.MenuItem MenuItem}
+     */
     @Override
     public List<TreeNode<MenuItem>> getMenuItems() {
         return model.getRoot().getChildren();
