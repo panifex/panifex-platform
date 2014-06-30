ac3edfeb5a35cb87341efd4802181adbf178498a
Mario Krizmanic <mario.krizmanic@gmail.com>
2014-06-30 21:23:53 +0200

    Fixes getting account's permissions
    Signed-off-by: Mario Krizmanic <mario.krizmanic@gmail.com>

--- panifex-persistence-impl/src/main/java/org/panifex/persistence/security/AccountRepositoryImpl.java
+++ panifex-persistence-impl/src/main/java/org/panifex/persistence/security/AccountRepositoryImpl.java
@@ -1,17 +1,17 @@
 /*******************************************************************************
  * Panifex platform
  * Copyright (C) 2013  Mario Krizmanic
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
@@ -35,24 +35,24 @@ import org.slf4j.LoggerFactory;
 public class AccountRepositoryImpl implements AccountRepository<AccountEntity> {
 
     private final Logger log = LoggerFactory.getLogger(AccountRepositoryImpl.class);
-    
+
     @Override
     public void insertAccount(EntityManager entityManager, AccountEntity account) {
         log.debug("Insert account: {}", account);
         /*
         AccountImplBuilder accountBuilder = new AccountImplBuilder(account);
-        
+
         AccountEntity accountImpl = accountBuilder.build();
-        
+
         entityManager.persist(accountImpl);*/
     }
-    
+
     @Override
     public void updateAccount(EntityManager entityManager, AccountEntity account) {
         log.debug("Update account: {}", account);
         entityManager.persist(account);
     }
-    
+
     /**
      * {@inheritDoc}
      */
@@ -62,17 +62,17 @@ public class AccountRepositoryImpl implements AccountRepository<AccountEntity> {
 
         CriteriaBuilder cb = entityManager.getCriteriaBuilder();
         CriteriaQuery<AccountEntity> cq = cb.createQuery(AccountEntity.class);
-        
+
         // select from account
         Root<AccountEntity> account = cq.from(AccountEntity.class);
         // where account.username = ?
         cq.where(cb.equal(account.get(AccountEntity_.username), username));
         cq.select(account);
-        
+
         // execute query
         TypedQuery<AccountEntity> q = entityManager.createQuery(cq);
         List<AccountEntity> accounts = q.getResultList();
-        
+
         if (accounts.size() == 1) {
             log.debug("Found account with username: {}", username);
             return accounts.get(0);
@@ -92,77 +92,79 @@ public class AccountRepositoryImpl implements AccountRepository<AccountEntity> {
     @Override
     public List<? extends Role> getRolesForAccount(EntityManager entityManager, AccountEntity account) {
         log.debug("Get roles by account: {}", account);
-        
+
         CriteriaBuilder cb = entityManager.getCriteriaBuilder();
         CriteriaQuery<RoleEntity> cq = cb.createQuery(RoleEntity.class);
-        
+
         // select from role
         Root<RoleEntity> role = cq.from(RoleEntity.class);
         cq.from(AccountRoleAssociationEntity.class);
         cq.from(AccountEntity.class);
-        
+
         // join account
         Join<RoleEntity, AccountRoleAssociationEntity> accountRoleAssociation =
                 role.join(RoleEntity_.accountRoleAssociations);
-        Join<AccountRoleAssociationEntity, AccountEntity> accounts = 
+        Join<AccountRoleAssociationEntity, AccountEntity> accounts =
                 accountRoleAssociation.join(AccountRoleAssociationEntity_.account);
-        
+
         // where account = ?
         cq.where(cb.equal(accounts.get(AccountEntity_.id), account.getId()));
-        
+
         // distinct
         cq.distinct(true);
         cq.select(role);
-        
+
         // execute query
         TypedQuery<RoleEntity> q = entityManager.createQuery(cq);
         List<RoleEntity> roles = q.getResultList();
-        
+
         return roles;
     }
-    
+
     /**
      * {@inheritDoc}
      */
     @Override
     public List<? extends Permission> getPermissionsForAccount(EntityManager entityManager, AccountEntity account) {
         log.debug("Get permission by account: {}", account);
-        
+
         CriteriaBuilder cb = entityManager.getCriteriaBuilder();
         CriteriaQuery<PermissionEntity> cq = cb.createQuery(PermissionEntity.class);
-        
-        // select from permission
-        Root<PermissionEntity> permission = cq.from(PermissionEntity.class);
-        
+
         // loads metamodel
         cq.from(AccountRoleAssociationEntity.class);
         cq.from(RolePermissionAssociationEntity.class);
         cq.from(RoleEntity.class);
         cq.from(AccountEntity.class);
-        
+
+        cq = cb.createQuery(PermissionEntity.class);
+
+        // select from permission
+        Root<PermissionEntity> permission = cq.from(PermissionEntity.class);
+
         // join role
-        Join<PermissionEntity, RolePermissionAssociationEntity> rolePermission = 
+        Join<PermissionEntity, RolePermissionAssociationEntity> rolePermission =
                 permission.join(PermissionEntity_.rolePermissionAssociations);
-        Join<RolePermissionAssociationEntity, RoleEntity> roles = 
+        Join<RolePermissionAssociationEntity, RoleEntity> roles =
                 rolePermission.join(RolePermissionAssociationEntity_.role);
-        Join<RoleEntity, AccountRoleAssociationEntity> accountRoleAssociation = 
+        Join<RoleEntity, AccountRoleAssociationEntity> accountRoleAssociation =
                 roles.join(RoleEntity_.accountRoleAssociations);
         Join<AccountRoleAssociationEntity, AccountEntity> accounts =
                 accountRoleAssociation.join(AccountRoleAssociationEntity_.account);
-               
+
         // where
         cq.where(cb.equal(accounts.get(AccountEntity_.id), account.getId()));
-        
+
         // distinct
-        cq.select(permission);
         cq.distinct(true);
-        
+        cq.select(rolePermission.getParent());
+
         // execute query
         TypedQuery<PermissionEntity> q = entityManager.createQuery(cq);
         List<PermissionEntity> permissions = q.getResultList();
-        
+
         log.debug("Returned {} permissions", permissions.size());
-        
+
         return permissions;
     }
 
