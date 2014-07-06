ff35aa2acf3630840e6e55eda1a5fb3295243286
Mario Krizmanic <mario.krizmanic@gmail.com>
2014-07-06 22:16:03 +0200

    Adds unit tests for DefaultPageletMapping class
    Signed-off-by: Mario Krizmanic <mario.krizmanic@gmail.com>

---
+++ panifex-module-api/src/test/java/org/panifex/module/api/DefaultPageletMappingTest.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * Panifex platform
+ * Copyright (C) 2013  Mario Krizmanic
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ ******************************************************************************/
+package org.panifex.module.api;
+
+import org.junit.Test;
+import org.panifex.test.support.TestSupport;
+
+/**
+ * Unit tests for {@link DefaultPageletMapping} class.
+ */
+public class DefaultPageletMappingTest extends TestSupport {
+
+    final String[] urlPatterns = new String[] { "/*" };
+
+    @Test
+    public void testConstructWithPageletName() {
+        String pageletName = "pageletName";
+
+        PageletMapping mapping = new DefaultPageletMapping(pageletName, urlPatterns);
+
+        assertEquals(pageletName, mapping.getPageletName());
+        assertEquals(urlPatterns, mapping.getUrlPatterns());
+    }
+
+    @Test
+    public void testConstructWithConcretePagelet() {
+        Pagelet<?, ?> pageletMock = createMock(Pagelet.class);
+
+        PageletMapping mapping = new DefaultPageletMapping(pageletMock, urlPatterns);
+
+        assertEquals(pageletMock.getClass().getCanonicalName(), mapping.getPageletName());
+        assertEquals(urlPatterns, mapping.getUrlPatterns());
+    }
+}
