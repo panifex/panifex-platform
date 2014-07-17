7f35bf44973102f841e3a8f149005bf09a044702
Mario Krizmanic <mario.krizmanic@gmail.com>
2014-07-17 13:43:31 +0200

    Ignored label locator unit tests because it will be replaced
    Signed-off-by: Mario Krizmanic <mario.krizmanic@gmail.com>

--- panifex-module-api/src/test/java/org/panifex/module/api/i18n/AbstractLocaleServiceTest.java
+++ panifex-module-api/src/test/java/org/panifex/module/api/i18n/AbstractLocaleServiceTest.java
@@ -1,17 +1,17 @@
 /*******************************************************************************
  * Panifex platform
  * Copyright (C) 2013  Mario Krizmanic
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
@@ -24,11 +24,11 @@ import java.util.HashSet;
 import java.util.Locale;
 import java.util.Set;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
-import org.panifex.module.api.i18n.AbstractLocaleService;
 import org.panifex.test.support.TestSupport;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
@@ -48,27 +48,28 @@ public final class AbstractLocaleServiceTest extends TestSupport {
     public void getSupportedLocaleTest() {
         // mocks
         Locale localeMock = createMock(Locale.class);
-        
+
         // perform test
         replayAll();
         AbstractLocaleService localeService = new AbstractLocaleServiceImpl(localeMock);
         Locale supportedLocale = localeService.supportedLocale();
         Set<URL> urls = localeService.locates();
         verifyAll();
-        
+
         // the locales must be the same
         assertNotNull(supportedLocale);
         assertEquals(localeMock, supportedLocale);
-        
+
         // the urls set must be empty because we hadn't associated any resource
         assertNotNull(urls);
         assertTrue(urls.isEmpty());
     }
-    
+
     /**
      * This test constructs a new AbstractLocaleService and associates two resources
      * and then it checks if the resources are successfully binded.
      */
+    @Ignore
     @Test
     public void bindResourcesTest() throws MalformedURLException {
         // variables
@@ -77,93 +78,93 @@ public final class AbstractLocaleServiceTest extends TestSupport {
         Set<String> resources = new HashSet<>();
         resources.add(resource1);
         resources.add(resource2);
-        
+
         // mocks
         Locale localeMock = createMock(Locale.class);
         BundleContext bundleContextMock = createMock(BundleContext.class);
         Bundle bundleMock = createMock(Bundle.class);
         URL url1Mock = new URL("http://url1");
         URL url2Mock = new URL("http://url2");
-        
+
         // expect getting the bundle
         expect(bundleContextMock.getBundle()).andReturn(bundleMock).times(2);
-        
+
         // expect getting the URLs of the resources
         expect(bundleMock.getResource(resource1)).andReturn(url1Mock);
         expect(bundleMock.getResource(resource2)).andReturn(url2Mock);
-        
+
         // perform test
         replayAll();
-        
+
         // construct the AbstractLocaleService
-        AbstractLocaleService localeService = 
+        AbstractLocaleService localeService =
                 new AbstractLocaleServiceImpl(localeMock, resources);
         localeService.setBundleContext(bundleContextMock);
-        
+
         Locale supportedLocale = localeService.supportedLocale();
         Set<URL> urls = localeService.locates();
         verifyAll();
-        
+
         // the locales must be the same
         assertNotNull(supportedLocale);
         assertEquals(localeMock, supportedLocale);
-        
+
         // the url collection must contain two urls
         assertNotNull(urls);
         assertEquals(2, urls.size());
         assertTrue(urls.contains(url1Mock));
         assertTrue(urls.contains(url2Mock));
     }
-    
+
     /**
      * This test constructs a new AbstractLocaleService and adds a resource
      * and then it checks if the resource is successfully binded.
      * <p>
-     * This test checks the functionality of {@link AbstractLocaleService#addResource(String)} 
+     * This test checks the functionality of {@link AbstractLocaleService#addResource(String)}
      * method.
      */
     @Test
     public void addResourceTest() throws MalformedURLException {
         // variables
         String resource = getRandomChars(20);
-        
+
         // mocks
         Locale localeMock = createMock(Locale.class);
         BundleContext bundleContextMock = createMock(BundleContext.class);
         Bundle bundleMock = createMock(Bundle.class);
         URL urlMock = new URL("http://url1");
-        
+
         // expect getting the bundle
         expect(bundleContextMock.getBundle()).andReturn(bundleMock);
-        
+
         // expect getting the URLs of the resources
         expect(bundleMock.getResource(resource)).andReturn(urlMock);
-        
+
         // perform test
         replayAll();
-        
+
         // construct the AbstractLocaleService
-        AbstractLocaleService localeService = 
+        AbstractLocaleService localeService =
                 new AbstractLocaleServiceImpl(localeMock);
         localeService.setBundleContext(bundleContextMock);
-        
+
         // add resource
         localeService.addResource(resource);
-        
+
         Locale supportedLocale = localeService.supportedLocale();
         Set<URL> urls = localeService.locates();
         verifyAll();
-        
+
         // the locales must be the same
         assertNotNull(supportedLocale);
         assertEquals(localeMock, supportedLocale);
-        
+
         // the url collection must contain a url
         assertNotNull(urls);
         assertEquals(1, urls.size());
         assertTrue(urls.contains(urlMock));
     }
-    
+
     /**
      * Tries to construct an AbstractLocaleService with unknown Locale.
      * <p>
@@ -184,7 +185,7 @@ public final class AbstractLocaleServiceTest extends TestSupport {
     public void constructNullResourceSetTest() {
         // mocks
         Locale localeMock = createMock(Locale.class);
-        
+
         // perform test
         new AbstractLocaleServiceImpl(localeMock, null);
     }
--- panifex-web-impl/src/test/java/org/panifex/web/impl/i18n/LabelLocatorImplTest.java
+++ panifex-web-impl/src/test/java/org/panifex/web/impl/i18n/LabelLocatorImplTest.java
@@ -1,17 +1,17 @@
 /*******************************************************************************
  * Panifex platform
  * Copyright (C) 2013  Mario Krizmanic
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
@@ -26,10 +26,10 @@ import java.util.Locale;
 
 import org.apache.commons.lang.StringUtils;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.panifex.test.support.TestSupport;
-import org.panifex.web.impl.i18n.LabelLocatorImpl;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
@@ -41,22 +41,22 @@ import org.powermock.modules.junit4.PowerMockRunner;
 public final class LabelLocatorImplTest extends TestSupport {
 
     private LabelLocatorImpl labelLocator;
-    
+
     // mocks
     private Locale locatorLocaleMock = createMock(Locale.class);
-    private URL locatorUrlMock = createMock(URL.class); 
-    
+    private URL locatorUrlMock = createMock(URL.class);
+
     /**
      * Prepares an environment for performing unit tests.
      */
     @Before
     public void setUp() {
         resetAll();
-        
+
         // construct LabelLocatorImpl
         labelLocator = new LabelLocatorImpl(locatorLocaleMock, locatorUrlMock);
     }
-    
+
     /**
      * This test tries to get a resource for the matched locale.
      * <p>
@@ -68,11 +68,11 @@ public final class LabelLocatorImplTest extends TestSupport {
         replayAll();
         URL url = labelLocator.locate(locatorLocaleMock);
         verifyAll();
-        
+
         // the resources must be the same
         assertEquals(locatorUrlMock, url);
     }
-    
+
     /**
      * This test tries to match the unknown locale. It is used
      * when the locale is not specified.
@@ -81,15 +81,15 @@ public final class LabelLocatorImplTest extends TestSupport {
     public void matchUnknownLocaleTest() {
         // expect getting the not specified language
         expect(locatorLocaleMock.getLanguage()).andReturn(StringUtils.EMPTY);
-        
+
         // perform test
         replayAll();
         URL url = labelLocator.locate(null);
         verifyAll();
-        
+
         assertEquals(locatorUrlMock, url);
     }
-    
+
     /**
      * This test tries to get a resource for the locale which
      * does not match the locator's.
@@ -100,22 +100,23 @@ public final class LabelLocatorImplTest extends TestSupport {
     public void notMatchedLocaleTest() {
         // mocks
         Locale localeMock = createMock(Locale.class);
-        
+
         // perform test
         replayAll();
         URL url = labelLocator.locate(localeMock);
         verifyAll();
-        
+
         // the resource must not be returned
         assertNull(url);
     }
-    
+
     /**
      * This test checks the equals contract.
-     * 
+     *
      * @see {@link java.lang.Object#equals(Object)}
      * @see {@link java.lang.Object#hashCode()}
      */
+    @Ignore
     @Test
     public void equalsContractTest() throws MalformedURLException {
         // mocks
@@ -123,21 +124,21 @@ public final class LabelLocatorImplTest extends TestSupport {
         Locale locale2Mock = new Locale("de");
         URL url1Mock = new URL("http://url1");
         URL url2Mock = new URL("http://url2");
-        
+
         // create locators
         List<LabelLocatorImpl> locators = new ArrayList<LabelLocatorImpl>();
-        
+
         LabelLocatorImpl locator1 = new LabelLocatorImpl(locale1Mock, url1Mock);
         locators.add(locator1);
         locators.add(new LabelLocatorImpl(locale1Mock, url2Mock));
         locators.add(new LabelLocatorImpl(locale2Mock, url1Mock));
         locators.add(new LabelLocatorImpl(locale2Mock, url2Mock));
-        
+
         LabelLocatorImpl locator2 = new LabelLocatorImpl(locale1Mock, url1Mock);
-        
+
         // verify equals contract
         assertEquals(locator1, locator2);
-        
+
         // verify not equals contract
         for (int first = 0 ; first < locators.size() ; first++) {
             for (int second = first + 1 ; second < locators.size() ; second++) {
@@ -146,6 +147,6 @@ public final class LabelLocatorImplTest extends TestSupport {
                 assertNotEquals(locators.get(second), locators.get(first));
             }
         }
-        
+
     }
 }
