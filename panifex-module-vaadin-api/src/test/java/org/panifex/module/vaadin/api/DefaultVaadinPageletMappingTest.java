6fdc889f130e15f6d62c991596468340872dcbef
Mario Krizmanic <mario.krizmanic@gmail.com>
2014-08-19 15:29:00 +0200

    Adds missing default pagelet mapping unit tests
    Signed-off-by: Mario Krizmanic <mario.krizmanic@gmail.com>

---
+++ panifex-module-vaadin-api/src/test/java/org/panifex/module/vaadin/api/DefaultVaadinPageletMappingTest.java
@@ -0,0 +1,53 @@
+/*******************************************************************************
+ * Panifex platform
+ * Copyright (C) 2013  Mario Krizmanic
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ ******************************************************************************/
+package org.panifex.module.vaadin.api;
+
+import org.junit.Test;
+import org.panifex.module.api.pagelet.PageletMapping;
+import org.panifex.test.support.TestSupport;
+
+/**
+ * Unit tests for {@link DefaultVaadinPageletMapping} class.
+ */
+public class DefaultVaadinPageletMappingTest extends TestSupport {
+
+    final String pageletName = "pageletName";
+    final String[] urlPatterns = new String[] { "/*" };
+
+    @Test
+    public void testConstructWithPageletName() {
+        PageletMapping mapping = new DefaultVaadinPageletMapping(pageletName, urlPatterns);
+
+        assertEquals(pageletName, mapping.getPageletName());
+        assertEquals(urlPatterns, mapping.getUrlPatterns());
+    }
+
+    @Test
+    public void testConstructWithConcretePagelet() {
+        VaadinPagelet pageletMock = createMock(VaadinPagelet.class);
+        expect(pageletMock.getName()).andReturn(pageletName);
+
+        replayAll();
+        PageletMapping mapping = new DefaultVaadinPageletMapping(pageletMock, urlPatterns);
+        verifyAll();
+
+        assertEquals(pageletName, mapping.getPageletName());
+        assertEquals(urlPatterns, mapping.getUrlPatterns());
+    }
+}
